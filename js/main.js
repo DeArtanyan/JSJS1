357ee0ac449d0c3837d0a2a81dfaa6d992ab5cf2
Стретович Владислав Евгеньевич <gr424_stvev@ttit.local>
2025-02-05 13:27:00 +0700

    fourth commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 let app = new Vue({
     el: '#app',
     data: {
-      product: 'Socks',
-      image: "./assets/vmSocks-green-onWhite.jpg",
-      inStock: true,
-      onSale: true
-    }   
-})
+    product: 'Socks',
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+
+    variants: [
+        {
+        variantId: 2234,
+        variantColor: 'green'    
+        },
+        {
+        variantId: 2235,
+        variantColor: 'blue'
+        }],
+        
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+}})
