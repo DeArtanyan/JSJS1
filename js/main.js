5e06a360e23ba0fe3f5e6a054cd82f3b516aeb6a
Стретович Владислав Евгеньевич <gr424_stvev@ttit.local>
2025-02-06 11:13:53 +0700

    tenth commit

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ Vue.component('product', {
             required: true
         }
     },
-    template:`
-<div class="product">  
-
+    template: `
+    <div class="product">
+          
         <div class="product-image">
             <img :src="image" />
         </div>
-
+  
         <div class="product-info">
             <h1>{{ product }}</h1>
             <p v-if="inStock">In Stock</p>
@@ -29,52 +29,64 @@ Vue.component('product', {
                 @mouseover="updateProduct(index)"
                 >
             </div> 
-
+  
             <button v-on:click="addToCart" 
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }"
-            >
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+                >
             Add to cart
             </button>
+  
+        </div> 
 
-        <button @click="removeFromCart" 
-        >
-        Remove from cart
-        </button>
-    </div>  
+            <div>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul v-else>
+                <li v-for="(review, index) in reviews" :key="index">
+                    <p>{{ review.name }}</p>
+                    <p>Rating:{{ review.rating }}</p>
+                    <p>{{ review.review }}</p>
+                </li>
+            </ul>
+        </div>
+        <product-review @review-submitted="addReview"></product-review>
 </div>
 `,
     data() {
-    return {
+        return {
         product: 'Socks',
         brand: 'Vue Mastery',
         selectedVariant: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+
         variants: [
             {
             variantId: 2234,
             variantColor: 'green',
-            variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
             variantQuantity: 10     
             },
             {
             variantId: 2235,
             variantColor: 'blue',
-            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
             variantQuantity: 0     
             }
-        ]
-    }},
+          ],
+        reviews: []
+        }
+    },
     methods: {
-        addToCart: function() {
+        addToCart() {
             this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
-        updateProduct: function(index) {  
+        updateProduct(index) {  
             this.selectedVariant = index
         },
-        removeFromCart: function() {
-                this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
-        }},
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        }
+    },
 
         computed: {
             title() {
@@ -91,25 +103,103 @@ Vue.component('product', {
                 return "Free"
             }
                 return 2.99
+        }
+    }
+})
+
+
+Vue.component('product-review', {
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+    
+        <p class="error" v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+
+        <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name">
+        </p>
+        
+        <p>
+            <label for="review">Review:</label>      
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+        
+        <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+            </select>
+        </p>
+
+        <p>Would you recommend this product?</p>
+        <label>
+            Yes
+            <input type="radio" value="Yes" v-model="recommend"/>
+        </label>
+        <label>
+            No
+            <input type="radio" value="No" v-model="recommend"/>
+        </label>
+            
+        <p>
+            <input type="submit" value="Submit">  
+        </p>    
+      
+    </form>
+    `,
+    data() {
+        return {
+        name: null,
+        review: null,
+        rating: null,
+        recommend: null,
+        errors: []
+        }
+    },
+
+    methods: {
+        onSubmit() {
+        this.errors = []
+        if(this.name && this.review && this.rating && this.recommend) {
+        let productReview = {
+            name: this.name,
+            review: this.review,
+            rating: this.rating,
+            recommend: this.recommend
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+            this.recommend = null
+        } else {
+            if(!this.name) this.errors.push("Name required.")
+            if(!this.review) this.errors.push("Review required.")
+            if(!this.rating) this.errors.push("Rating required.")
+            if(!this.recommend) this.errors.push("Recommendation required.")
+        }
     }}
 })
   
 let app = new Vue({
-      el: '#app',
-        data: {
-            premium: true,
-            cart: []
-        },
-      
-    methods: {
+    el: '#app',
+    data: {
+        premium: true,
+        cart: []
+    },
+
+        methods: {
         updateCart(id) {
             this.cart.push(id)
-        },
-        removeItem(id) {
-        for(var i = this.cart.length - 1; i >= 0; i--) {
-            if (this.cart[i] === id) {
-                this.cart.splice(i, 1);
-            }
-        }}
+        }
     }
 })
