41b5afebde0af09956bc3fec0fb9dcb9985bb842
Стретович Владислав Евгеньевич <gr424_stvev@ttit.local>
2025-02-06 10:31:39 +0700

    eighth commit

--- js/main.js
+++ js/main.js
@@ -1,40 +1,26 @@
-Vue.component('product-details', {
-    props: {
-        details: {
-            type: Array,
-            required: true
-        }
-    },
-    template: `
-        <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-    `
-})
-  
-  
-  
-Vue.component('product',{
+Vue.component('product', {
     props: {
         premium: {
             type: Boolean,
             required: true
         }
     },
-    template: `
-    <div class="product">
-          
+    template:`
+<div class="product">  
+
         <div class="product-image">
             <img :src="image" />
         </div>
-  
+
         <div class="product-info">
             <h1>{{ product }}</h1>
             <p v-if="inStock">In Stock</p>
             <p v-else>Out of Stock</p>
             <p>Shipping: {{ shipping }}</p>
   
-            <product-details :details="details"></product-details>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
   
             <div class="color-box"
                 v-for="(variant, index) in variants" 
@@ -43,31 +29,28 @@ Vue.component('product',{
                 @mouseover="updateProduct(index)"
                 >
             </div> 
-  
+
             <button v-on:click="addToCart" 
-                :disabled="!inStock"
-                :class="{ disabledButton: !inStock }"
-                >
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
             Add to cart
             </button>
-  
-            <div class="cart">
-              <p>Cart({{ cart }})</p>
-            </div>
-  
-        </div>  
-      
-    </div>
-    `,
 
+        <button @click="removeFromCart" 
+        >
+        Remove from cart
+        </button>
+    </div>  
+</div>
+`,
     data() {
-      return {
-          product: 'Socks',
-          brand: 'Vue Mastery',
-          selectedVariant: 0,
-          details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-
-          variants: [
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
             {
             variantId: 2234,
             variantColor: 'green',
@@ -80,42 +63,53 @@ Vue.component('product',{
             variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             variantQuantity: 0     
             }
-          ],
-            cart: 0
-        }
-    },
-
+        ]
+    }},
     methods: {
         addToCart: function() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
         updateProduct: function(index) {  
             this.selectedVariant = index
-        }
-    },
+        },
+        removeFromCart: function() {
+                this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+        }},
 
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product  
-        },
-        image(){
-            return this.variants[this.selectedVariant].variantImage
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-          },
-        shipping() {
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product  
+            },
+            image(){
+                return this.variants[this.selectedVariant].variantImage
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].variantQuantity
+            },
+            shipping() {
             if (this.premium) {
                 return "Free"
             }
                 return 2.99
-        }
-    }
+    }}
 })
   
 let app = new Vue({
-    el: '#app',
-    data: {
-        premium: true
+      el: '#app',
+        data: {
+            premium: true,
+            cart: []
+        },
+      
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeItem(id) {
+        for(var i = this.cart.length - 1; i >= 0; i--) {
+            if (this.cart[i] === id) {
+                this.cart.splice(i, 1);
+            }
+        }}
     }
 })
